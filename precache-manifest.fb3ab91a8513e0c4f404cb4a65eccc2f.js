92d24b1dac7ec2f5429d666993cc20793f38850f
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-10-22 13:47:51 -0400

    First commit

---
+++ precache-manifest.fb3ab91a8513e0c4f404cb4a65eccc2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4219bc588a3436d6068032f52b834eb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8113769ac5bf0ff3336a",
+    "url": "/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "ebc37115e32df7a04376",
+    "url": "/static/js/2.5be921a9.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.5be921a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8113769ac5bf0ff3336a",
+    "url": "/static/js/main.1029ecb1.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
